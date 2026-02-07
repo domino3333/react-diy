751f03e21ab95d538ece29fcf6ade0ce5095fc99
Minho Kang <busster@naver.com>
2026-02-07 14:59:47 +0900

    삼항연산으로 태그 리턴하기

--- prac01/src/components/Main.jsx
+++ prac01/src/components/Main.jsx
@@ -1,10 +1,11 @@
 import '../css/Main.css'
 
-function Main(){
+function Main() {
 
-    const user ={
-        ["name:"]:"강민호",
-        ['age']:23,
+    const user = {
+        ["name:"]: "강민호",
+        ['age']: 23,
+        isLogin: true,
     }
 
     return (<>
@@ -12,6 +13,9 @@ function Main(){
         <h1 className="h1text">여긴 메인</h1>
         <p>내이름은 {user.name}</p>
         <p>나이는 {user.age}</p>
+        <p>로그인/로그아웃 표시</p>
+        {user.isLogin === true ? (<p>로그인 중</p>) : (<p>로그아웃 상태</p>)}
+
 
     </>)
 }
