7915cf861aeb706984806d110fba4043334db43f
Minho Kang <busster@naver.com>
2026-02-09 02:00:49 +0900

    화면 구조, css 기초 완성

---
+++ todo-app-diy/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import '../css/Header.css'
+
+const Header = ()=>{
+ 
+    return(<>
+        <div className="Header">
+            <h2>오늘의 할 일</h2>
+
+        </div>
+    </>)
+}
+
+export default Header;
---
+++ todo-app-diy/src/components/List.jsx
@@ -0,0 +1,12 @@
+
+const List = ()=>{
+ 
+    return(<>
+        <div className="List">
+            List
+
+        </div>
+    </>)
+}
+
+export default List;
