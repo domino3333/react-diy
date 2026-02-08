552539274fca41287cb0417ecf00c5117f7e16ac
Minho Kang <busster@naver.com>
2026-02-08 18:00:10 +0900

    실제로 inputRef에 어떤 값이 들어가는지 확인

--- prac01/src/components/MyComponent.jsx
+++ prac01/src/components/MyComponent.jsx
@@ -1,16 +1,25 @@
-import { useRef } from 'react';
+import { useEffect, useRef,useState} from 'react';
 
 export default function MyComponent() {
   const inputRef = useRef(null);
+  const [count,setCount] = useState(0);
 
   function handleClick() {
     inputRef.current.focus();
+    setCount(count+1);
+
   }
+  
+
+  useEffect(()=>{
+    console.log(inputRef);
+  })
 
   return (
     <>
       <input ref={inputRef} />
       <button onClick={handleClick}>포커스</button>
+      <p>{count}</p>
     </>
   );
 }
