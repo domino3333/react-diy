46eed8aedf2a3ca8d632d425ffe8301985187ce5
Minho Kang <busster@naver.com>
2026-02-12 01:12:03 +0900

    [router예습] query param / path param 구분해서 사용하기 query는 보통 키:밸류, path는 board/3 처럼 숫자로 끝

--- routerPrac/src/pages/New.jsx
+++ routerPrac/src/pages/New.jsx
@@ -1,4 +1,37 @@
+
+import { useSearchParams } from "react-router-dom";
+
 const New = () => {
- return <div>New</div>;
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    //?asd=Alice로 넘어온 값을 name으로 저장해서
+    // 랜더링 때 쓰려는 것임
+    const name = searchParams.get('asd');
+
+    const updateNameParam = (newName) => {
+        setSearchParams({ asd: newName });
+    }
+
+
+
+    return (<>
+        <h1>Hello, {name || 'Guest'}</h1>
+        {/*이 버튼을 누르면 주소가 new?name=alice로 설정됨 */}
+        <button onClick={() => updateNameParam('Alice')}>
+            Set name to Alice
+        </button>
+        <button onClick={() => updateNameParam('Bob')}>
+            Set name to Bob
+        </button>
+
+    </>)
+
+
+
+
+
+
+
 };
 export default New;
