81ee0c8b5f6768c239dd8657f4799c7beab103e8
Minho Kang <busster@naver.com>
2026-02-12 00:46:19 +0900

    [router예습] 라우터 매핑하고 Link 걸어주는 방법 테스트

--- routerPrac/src/App.jsx
+++ routerPrac/src/App.jsx
@@ -1,10 +1,39 @@
 import './App.css'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Notfound from "./pages/Notfound"
 
 const App = () => {
+
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/new");
+  }
+  <button onClick={onClickButton}>
+    New 페이지로 이동
+  </button>
   return (
     <>
-    <div>Hello</div>
+      <div>Hello</div>
+      <Link to={"/"}>Home</Link><br />
+      <Link to={"/new"}>New</Link><br />
+      <Link to={"/diary"}>Diary</Link><br />
+      <Link to={'/edit'}>Edit</Link><br />
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="*" element={<Notfound />} />
+
+      </Routes>
+      <button onClick={onClickButton}>
+        New 페이지로 이동
+      </button >
     </>
   )
 }
