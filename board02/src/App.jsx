f8cd7815b86a1d200c6581bcc579e4d7010355e7
Minho Kang <busster@naver.com>
2026-02-14 22:47:15 +0900

    change 값 읽히는지 테스트

--- board02/src/App.jsx
+++ board02/src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import './App.css'
-
 function App() {
 
+
+  const [state, setState] = useState();
+
+  const observeNameInput = (e) => {
+    setState(e.target.value)
+  }
+  console.log(state)
   return (
     <>
+      <input type="text" name="name" id="name" onChange={observeNameInput} />
     </>
   )
 }
